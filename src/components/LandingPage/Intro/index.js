b4c16b14eacd8b9870d9ddcbb7bf4adf88f221dc
Biswa Saraf <117895834+Biswa4414@users.noreply.github.com>
2023-11-05 10:57:21 +0530

    Update index.js

--- src/components/LandingPage/Intro/index.js
+++ src/components/LandingPage/Intro/index.js
@@ -47,7 +47,7 @@ function LandingPageComponent() {
           <RWebShare            // used for sharing app
             data={{
               text: "Crypto Dashboard made using React JS.",
-              url: "https://crypto-dashboard-dec.netlify.app/",
+              url: "https://crypto-tracker-by-biswa.vercel.app/",
               title: "CryptoDashboard.",
             }}
             onClick={() => console.log("shared successfully!")}
